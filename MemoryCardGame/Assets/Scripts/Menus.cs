a54bfa08a9b64295726df863f0afde06cad912f9
Adriel Oliveira <richardd.oci12@gmail.com>
2016-05-25 20:52:26 -0300

    ToyBox

---
+++ MemoryCardGame/Assets/Scripts/Menus.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Menus : MonoBehaviour {
+
+	public void NextScene(string nextScene){
+		Application.LoadLevel(nextScene);
+	}
+
+	public void Exit(){
+		Application.Quit();
+	}
+
+
+}
---
+++ MemoryCardGame/Assets/Scripts/Win.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Win : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Awake () {
+		anim = GetComponent<Animator> ();
+	}
+
+}
